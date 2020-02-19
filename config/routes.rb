da7a206a9b6689345ab57b998850750382348650
Jorge Vargas <jvargas18@ucol.mx>
2020-02-18 18:00:12 -0600

    Add user authentication with devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, path: "", path_names: { sign_in: "login", sign_out: "logout" }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "home#index"
+
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+  end
+
+  match "*path" => redirect("/"), :via => [:get, :post]
 end
