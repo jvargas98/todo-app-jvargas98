09189b0f03ac91a22a14b965c484bf20c8f6394b
Jorge Vargas <jvargas18@ucol.mx>
2020-03-03 23:31:40 -0600

    Update user views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description, :email, :password])
+    devise_parameter_sanitizer.permit(:edit, keys: [:first_name, :last_name, :description, :email, :password])
+  end
 end
