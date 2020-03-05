b124999c4e3d80501756925b64da9bca749b75a2
Jorge Vargas <jvargas18@ucol.mx>
2020-03-04 20:10:27 -0600

    Change devise test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,7 +62,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers
 end
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
